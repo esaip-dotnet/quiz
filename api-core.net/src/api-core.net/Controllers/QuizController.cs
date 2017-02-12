00eca063152bf61521e9986a0927c25ad6d41a0c
Florian <fdonaoni.ir2017@esaip.org>
2017-02-12 14:08:42 +0100

    docs: commentaire des controllers

--- api-core.net/src/api-core.net/Controllers/QuizController.cs
+++ api-core.net/src/api-core.net/Controllers/QuizController.cs
@@ -8,9 +8,15 @@ using System.Threading.Tasks;
 
 namespace api_core.net.Controllers
 {
+    /**
+     * Controller de la classe Quiz
+     * La route de base du controller est :
+     * quiz
+     **/
     [Route("quiz")]
     public class QuizController : Controller
     {
+        // Le DAO nécessaire au traitement des données
         QuizDao quizDao;
 
         public QuizController(QuizDao quizDao)
@@ -18,6 +24,14 @@ namespace api_core.net.Controllers
             this.quizDao = quizDao;
         }
 
+        /**
+         * Route GET
+         * Renvoie un objet Quiz 
+         * HTTP Code : 200
+         * 
+         * @param idQuiz id du quiz
+         * @return un objet Quiz
+         **/
         [HttpGet("{idQuiz}")]
         public IActionResult GetId(string idQuiz)
         {
@@ -29,6 +43,13 @@ namespace api_core.net.Controllers
             return new ObjectResult(quiz);
         }
 
+        /**
+         * Route GET
+         * Renvoie tous les quiz 
+         * HTTP Code : 200
+         * 
+         * @return list d'objet Quiz
+         **/
         [HttpGet]
         public IActionResult GetAll()
         {
@@ -40,6 +61,13 @@ namespace api_core.net.Controllers
             return new ObjectResult(listQuiz);
         }
 
+        /**
+         * Route POST
+         * Créé un objet Quiz et l'URL de cette dernière
+         * HTTP Code : 201
+         * 
+         * @param quiz un objet Quiz
+         **/
         [HttpPost]
         public async Task<IActionResult> Post([FromBody]Quiz quiz)
         {
@@ -48,6 +76,14 @@ namespace api_core.net.Controllers
             return new CreatedResult($"/quiz/{quiz.Id}", quiz);
         }
 
+        /**
+         * Route PATCH
+         * PATCH un objet quiz
+         * HTTP Code : 200
+         * 
+         * @param idQuiz id de la quiz
+         * @param patch l'objet quiz sous forme de JSONPatchDocument
+         **/
         [HttpPatch("{idQuiz}")]
         public async Task<IActionResult> Patch(string idQuiz, [FromBody]JsonPatchDocument<Quiz> patch)
         {
