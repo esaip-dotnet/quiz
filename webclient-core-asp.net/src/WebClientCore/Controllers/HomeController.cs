b334a3e5ed033de8fd7273e35c2927b5e14aa95b
TonyJallais <tony.jallais@gmail.com>
2017-02-17 17:16:55 +0100

    Ajout des fichiers
    Après suppression des fichiers précédents, ajout des nouveaux fichiers.

---
+++ webclient-core-asp.net/src/WebClientCore/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebClientCore.Models;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net;
+using WebClientCore.Extensions;
+
+namespace WebClientCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("Home")]
+        [Route("")]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Ajouter le quiz
+        [HttpPost]
+        [Route("Home/Validate")]
+        public IActionResult Validate(Quiz quiz)
+        {
+            Uri resultHttp = GetResponseUri(quiz);
+
+            if (resultHttp == null) {
+                return RedirectToAction("Index", "Home");
+            }
+            else {
+                try {
+                    return RedirectToAction("Index", "Question", new { quizUri = resultHttp });
+                } catch (AggregateException e)  {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+
+        //Récupérer la réponse de la méthode POST du quiz
+        private Uri GetResponseUri(Quiz quiz)
+        {
+            var client = new HttpClient();
+            var response = HttpClientEx.PostJsonAsync(client, "http://coreosjpg.cloudapp.net:80/quiz", quiz).GetAwaiter().GetResult(); ;
+            if (response.StatusCode == HttpStatusCode.Created)
+            {
+                var contents = response.Headers.Location;
+                return contents;
+            }
+            return null;
+        }
+    }
+}
