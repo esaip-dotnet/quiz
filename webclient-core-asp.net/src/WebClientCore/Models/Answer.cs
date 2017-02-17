261843800d1d48de1e799b2eb49822def81eec32
TonyJallais <tony.jallais@gmail.com>
2017-02-17 19:45:21 +0100

    Ajout de commentaire

--- webclient-core-asp.net/src/WebClientCore/Models/Answer.cs
+++ webclient-core-asp.net/src/WebClientCore/Models/Answer.cs
@@ -7,14 +7,20 @@ using System.Threading.Tasks;
 
 namespace WebClientCore.Models
 {
+    //Objet answer
     public class Answer
     {
+        //Nécessaire
+        //Avec la propriété json : title
         [Required]
         [JsonProperty("title")]
         public string Title { get; set; }
+        //Nécessaire
+        //Avec la propriété json : correct
         [Required]
         [JsonProperty("correct")]
         public bool Correct { get; set; }
+        //Avec la propriété json : picture
         [JsonProperty("picture")]
         public string Picture { get; set; }
     }
--- webclient-core-asp.net/src/WebClientCore/Models/Patch.cs
+++ webclient-core-asp.net/src/WebClientCore/Models/Patch.cs
@@ -6,15 +6,20 @@ using System.Threading.Tasks;
 
 namespace WebClientCore.Models
 {
+    //Object pour créer le patch
     public class Patch
     {
+        //Avec la propriété json : op
         [JsonProperty("op")]
         public string Operation { get; set; }
+        //Avec la propriété json : path
         [JsonProperty("path")]
         public string Path { get; set; }
+        //Avec la propriété json : value
         [JsonProperty("value")]
         public Question Question { get; set; }
 
+        //Constructeur
         public Patch(string Operation, string Path, Question Question) {
             this.Operation = Operation;
             this.Path = Path;
