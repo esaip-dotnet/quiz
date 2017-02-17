8e821e24a82450205fe8f07ef7a458ed181d50c3
arry61 <arry_16@hotmail.com>
2017-02-17 18:17:27 +0100

    ADD comments
    Ajout de commentaires dans les deux projets windows-phone et
    réorganisation du code pour le rendre plus propre.

--- client/windows-phone/Appli2 - LectureJson/App2/MainPage.xaml.cs
+++ client/windows-phone/Appli2 - LectureJson/App2/MainPage.xaml.cs
@@ -17,9 +17,6 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-
-// Pour en savoir plus sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=391641
-
 namespace App2
 {
     /// <summary>
@@ -30,80 +27,50 @@ namespace App2
         public MainPage()
         {
             this.InitializeComponent();
-
             this.NavigationCacheMode = NavigationCacheMode.Required;
         }
 
-        /// <summary>
-        /// Invoqué lorsque cette page est sur le point d'être affichée dans un frame.
-        /// </summary>
-        /// <param name="e">Données d’événement décrivant la manière dont l’utilisateur a accédé à cette page.
-        /// Ce paramètre est généralement utilisé pour configurer la page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             // TODO: préparer la page pour affichage ici.
-
-            // TODO: si votre application comporte plusieurs pages, assurez-vous que vous
-            // gérez le bouton Retour physique en vous inscrivant à l’événement
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed.
-            // Si vous utilisez le NavigationHelper fourni par certains modèles,
-            // cet événement est géré automatiquement.
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            SubmitData();
-
-
+        {//si on clique sur le bouton "valider"...
+            SubmitData(); //on lance la fonction SubmitData()
         }
 
         private async void SubmitData()
         {
             try{
-                //String answer = reponse1.Name.ToString(); //checkbox pas du text
-                //string putData = "answer" + answer;
+                //création d'une variable de stockage pour le json
                 System.IO.Stream src = Application.GetResourceStream(new Uri("Participation.json", UriKind.Relative)).Stream;
                 using (StreamReader json = new StreamReader(src))
                 {
-                    String putData=json.ReadToEnd();
+                    String putData=json.ReadToEnd(); //Lecture du fichier json, et stockage dans une variable putData
 
-                    //HTTP web request
+                    //création d'une HTTP web request
                     WebRequest request = WebRequest.Create("http://coreosjpg.cloudapp.net/quiz/1234/participation/1234567");
                     request.Method = "PUT";
                     request.ContentType = "text/json";
-                    //request.ContentLength = data.Length;
 
-                    //traitement de la requete
+                    //traitement de la requete et envoi de putData
                     using (var stream = await request.GetRequestStreamAsync())
                     {
                         var datas = Encoding.UTF8.GetBytes(putData);
                         await stream.WriteAsync(datas, 0, datas.Length);
-                        
-
                     }
                 }
-              
-                //String json = StreamReader.(@"C:\Users\alexandre\Desktop\App2\App2\Participation.json");
+            }
 
-            } 
-            catch(Exception ex) { }
-
-        }
-
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
+            catch(Exception ex) { 
+                //Gestion des exceptions
+            }
         }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
-
+            //Dans le cas où un des radio button est sélectionné...
         }
-
     }
 }
