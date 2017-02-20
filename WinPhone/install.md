8114b20c291848176947088432ca5364f75ad7f6
arry61 <arry_16@hotmail.com>
2017-02-20 23:26:38 +0100

    feat: ajout de winphone & web core
    Ajout de la nouvelle application winphone et du nouveau client web core

---
+++ WinPhone/install.md
@@ -0,0 +1,15 @@
+#Installation d'un windows phone
+
+##Via Visual Studio
+
+Branchez votre appareil Windows Phone (version 8.0) sur un PC équipé de Visual Studio
+Lancez l'application (l'extension devrait être de type .sln)
+Déployez l'application sur votre appareil
+
+TADAM ! Vous pouvez désormais vous instruire tout en vous amusant.
+
+##Via Windows Store
+
+Malheureusement notre application n'est pas encore disponible sur cette plateforme :( !
+
+Mais retentez votre chance bientôt, cela ne saurait tarder :) !
---
+++ web-client-core-asp.net/src/WebClientCore/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebClientCore.Models;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net;
+using WebClientCore.Extensions;
+
+//Controller pour la page d'ajout de quiz (page d'accueil)
+namespace WebClientCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Nous pouvons accéder à cette page directement en accédant au site
+        //Il est aussi possible d'y accéder en mettant /home et /index
+        [Route("Home")]
+        [Route("")]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            //Affichage de la page
+            return View();
+        }
+
+        //Méthode POST permettant d'ajouter un quiz
+        //Dès que l'utilisateur valide le formulaire de la page d'accueil
+        [HttpPost]
+        [Route("Home/Validate")]
+        public IActionResult Validate(Quiz quiz)
+        {
+            //Récupérer l'uri de la réponse de l'api
+            //Il faut mettre en paramètre le quiz à ajouter
+            Uri resultHttp = GetResponseUri(quiz);
+
+            //Si nous ne recevons pas de réponse
+            if (resultHttp == null) {
+                //Retourner sur la page principale
+                return RedirectToAction("Index", "Home");
+            }//Sinon si nous recevons une réponse
+            else {
+                try {
+                    //On essaye d'accéder à la page d'ajout de question avec l'url récupérée
+                    return RedirectToAction("Index", "Question", new { quizUri = resultHttp });
+                } catch (AggregateException e)  {
+                    //Si il y a une exception, retourner sur la page d'accueil
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+
+        //Récupérer la réponse de la méthode POST du quiz
+        private Uri GetResponseUri(Quiz quiz)
+        {
+            //Initialition de la variable client avec un HttpClient
+            var client = new HttpClient();
+            //Attendre la réponse de l'api
+            //Si la réponse est 201 c'est que le quiz a bien été créé
+            var response = HttpClientEx.PostJsonAsync(client, "http://coreosjpg.cloudapp.net:80/quiz", quiz).GetAwaiter().GetResult();
+            //Vérifier si le code est 201
+            if (response.StatusCode == HttpStatusCode.Created) {
+                //Si oui retourner l'uri du quiz
+                var contents = response.Headers.Location;
+                return contents;
+            }
+            return null;
+        }
+    }
+}
