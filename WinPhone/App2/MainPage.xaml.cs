47da7820994444e0aee6d8ca8a3e9a8a62a2d2cb
arry61 <arry_16@hotmail.com>
2017-02-20 23:46:39 +0100

    feat(winphone): API image
    Ajout d'une API permettant d'afficher une image récupérée via l'API sur
    la MainPage de l'application

--- WinPhone/App2/MainPage.xaml.cs
+++ WinPhone/App2/MainPage.xaml.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Windows;
-using System.Text;
-using System.Threading.Tasks;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -15,17 +12,46 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
-using Windows.Storage;
+
+// Pour en savoir plus sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=391641
 
 namespace ESAIP_Quiz
 {
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+            this.LoadImage();
             this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        void LoadImage()
+        {
+            Uri myUri = new Uri("http://thecatapi.com/api/images/get?format=src&type=jpg", UriKind.Absolute);
+            BitmapImage bmi = new BitmapImage();
+            bmi.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+            bmi.UriSource = myUri;
+            image.Source = bmi;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: préparer la page pour affichage ici.
+        }
+
+        private void Valider_Click(object sender, RoutedEventArgs e)
+        {
+            Uri myUri = new Uri("http://thecatapi.com/api/images/get?format=src&type=jpg", UriKind.Absolute);
+            BitmapImage bmi = new BitmapImage();
+            bmi.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+            bmi.UriSource = myUri;
+            image.Source = bmi;
         }
     }
 }
